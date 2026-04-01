a56e36455d9a320a26d1ca6f88a1b290de30dc7b
Elias Yalikun <elias.yalikun@gmail.com>
2026-04-01 13:43:32 +0200

    Fix diameter values for AXAL-TT 3X150/35, AXAL-TT 3X95/35, AXAL-TT 3X50/25, and AXAL-TT 3X25/25 in standardVal

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const standardVal = {
     { namn: "N1XE-AR 4G25", diameter: 0.025 },
     { namn: "AXAL-TT 3X300/50", diameter: 0.075 },
     { namn: "AXAL-TT 3X240/5", diameter: 0.070 },
-    { namn: "AXAL-TT 3X150/35", diameter: 0.063 },
-    { namn: "AXAL-TT 3X95/35", diameter: 0.055 },
-    { namn: "AXAL-TT 3X50/25", diameter: 0.050 },
-    { namn: "AXAL-TT 3X25/25", diameter: 0.045 },
+    { namn: "AXAL-TT 3X150/35", diameter: 0.052 },
+    { namn: "AXAL-TT 3X95/35", diameter: 0.047 },
+    { namn: "AXAL-TT 3X50/25", diameter: 0.040 },
+    { namn: "AXAL-TT 3X25/25", diameter: 0.036 },
     { namn: "OPTO Kabel", diameter: 0.040 }
   ],
   ror: [
